38d1d7e945c05399157417b8f8ccf15911aea4fb
MuhammadAzril-RPL <mhmdazril1724@gmail.com>
2025-03-07 20:53:02 +0700

    first commit

---
+++ CelciusToFahren/src/celciustofahren/CelciusToFahren.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package celciustofahren;
+
+/**
+ *
+ * @author acer
+ */
+public class CelciusToFahren {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ MuhammadAzril_539231217/src/Halaman/layout/CardLayout.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Halaman.layout;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author acer
+ */
+class CardLayout {
+
+    void show(JPanel Page1, String string) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
